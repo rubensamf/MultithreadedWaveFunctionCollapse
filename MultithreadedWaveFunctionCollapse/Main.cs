202b5cfa200eddef03cbcf6a63379f515dd3df7a
rubensamf <rubensamf@utexas.edu>
2017-08-11 09:30:33 -0700

    Reworked output

--- MultithreadedWaveFunctionCollapse/Main.cs
+++ MultithreadedWaveFunctionCollapse/Main.cs
@@ -19,20 +19,23 @@ static class Program
 {
     private static Random rand;
     private static XmlDocument xdoc;
-    private static int seed, num_trials, num_main_loop_iterations;
+    private static int base_seed, num_tries, num_trials, seed;
     private static bool write_images;
     private static string[] execution;
     private static string cur_execution;
-    private static int[] max_degree_parallelism, num_workers;
+    private static int[] max_degree_parallelism, num_workers, replicate_seeds;
     private static int cur_max_degree_parallelism, cur_num_workers;
     private static int SCALE_FACTOR;
+    //private static readonly int NUM_REPLICATE_SEEDS = 10;
 
     static void Main(string[] args)
     {
+        // Command line arguments: bool_write_images int_seed int_number_of_trials int_number_of_tries scale_factor [execution_mode int_max_degree_of_parallelism int_number_of_workers] ...
         InitializeAndLoad(args);
-        List<string[]> runtimes = new List<string[]>(num_main_loop_iterations);
-        for (int i = 0; i < num_main_loop_iterations; i++)
+        List<string[]> runtimes = new List<string[]>(num_trials);
+        for (int i = 0; i < num_trials; i++)
         {
+            seed = replicate_seeds[i];
             runtimes.Add(DisplayTime(Execute(), i));
         }
 
@@ -40,107 +43,11 @@ static class Program
         WaitForEscKey();
     }
 
-    private static void InitializeAndLoad(string[] args)
-    {
-        try
-        {
-            write_images = Convert.ToBoolean(args[0]);
-            seed = Convert.ToInt32(args[1]);
-            num_main_loop_iterations = Convert.ToInt32(args[2]);
-            num_trials = Convert.ToInt32(args[3]);
-            SCALE_FACTOR = Convert.ToInt32(args[4]);
-
-            int num_execution_modes = (args.Length - 5) / 3;
-
-            execution = new string[num_execution_modes];
-            max_degree_parallelism = new int[num_execution_modes];
-            num_workers = new int[num_execution_modes];
-
-            int i = 0;
-            for (int j = 5; j < args.Length; j += 3)
-            {
-                execution[i] = args[j];
-                max_degree_parallelism[i] = Convert.ToInt32(args[j + 1]);
-                num_workers[i] = Convert.ToInt32(args[j + 2]);
-                ValidExecutionName(execution[i], j);
-                i++;
-            }
-
-            rand = new Random(seed);
-            xdoc = new XmlDocument();
-
-            xdoc.Load("samples.xml");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Usage: MultithreadedWaveFunctionCollapse bool_write_images int_seed int_number_of_main_loop_iterations int_number_of_trials scale_factor " +
-                "execution_mode int_max_degree_of_parallelism int_number_of_workers ... " +
-                "(execution_mode int_max_degree_of_parallelism int_number_of_workers)");
-            WaitForEscKey();
-            Environment.Exit(exitCode: 24);
-        }
-    }
-
-    private static bool ValidExecutionName(string v, int j)
-    {
-        switch (v)
-        {
-            case "sequential-main":
-                return true;
-            case "parallel-main":
-                return true;
-            case "parallel-propagate":
-                return true;
-            case "parallel-observe":
-                return true;
-            default:
-                Console.WriteLine(j + "th argument " + v + " is invalid");
-                Console.WriteLine("Use only one of the following: sequential-main, parallel-main, parallel-propagate, or parallel-observe");
-                Environment.Exit(1);
-                return false;
-        }
-    }
-
-    private static string[] DisplayTime(TimeSpan[] execution_times, int run_num)
-    {
-        Console.WriteLine();
-        ulong total_runtime = 0;
-        string[] lines = new string[execution.Length];
-        for (int i = 0; i < execution.Length; i++)
-        {
-            Console.WriteLine(execution[i] + " execution time: " + execution_times[i]);
-            lines[i] = run_num + ", " + SCALE_FACTOR + ", " + execution[i] + ", " + seed + ", " + max_degree_parallelism[i] + ", " +
-                num_workers[i] + ", " + num_trials + ", " + num_main_loop_iterations + ", " + execution_times[i];
-            total_runtime += (ulong) execution_times[i].TotalMilliseconds;
-        }
-
-        Console.WriteLine("Run #" + run_num + " Runtime: " + total_runtime + " ms\n");
-
-        return lines;
-    }
-
-    private static void WriteRuntimes(List<string[]> runs)
-    {
-        StringBuilder sb = new StringBuilder("Run Number, Scale Factor, Execution Name, Seed, Max Degree of Parallelism, Number of Workers, Number of Trials, Number of Main Loop Iterations, Runtime (ms)\n");
-
-        foreach (var runtimes in runs)
-        {
-            foreach (var runtime in runtimes)
-            {
-                sb.Append(runtime + "\n");
-            }
-        }
-
-        string path = Directory.GetCurrentDirectory() + "\\" + "MultithreadedWaveFunctionCollapseResults.csv";
-        System.IO.File.WriteAllText(path, sb.ToString());
-    }
-
     private static TimeSpan[] Execute()
     {
         TimeSpan[] runtimes = new TimeSpan[execution.Length];
         for (int i = 0; i < execution.Length; i++)
         {
-            rand = new Random(seed);
             cur_execution = execution[i];
             cur_max_degree_parallelism = max_degree_parallelism[i];
             cur_num_workers = num_workers[i];
@@ -183,22 +90,130 @@ static class Program
         switch (cur_execution)
         {
             case "parallel-main":
-                wfc = new ParallelMain(num_trials, rand, write_images, cur_num_workers, SCALE_FACTOR, seed);
+                wfc = new ParallelMain(num_tries, rand, write_images, cur_num_workers, SCALE_FACTOR, seed);
                 break;
             case "parallel-propagate":
-                wfc = new ParallelPropagate(num_trials, rand, write_images, cur_max_degree_parallelism, SCALE_FACTOR, seed);
+                wfc = new ParallelPropagate(num_tries, rand, write_images, cur_max_degree_parallelism, SCALE_FACTOR, seed);
                 break;
             case "parallel-observe":
-                wfc = new ParallelObserve(num_trials, rand, write_images, cur_max_degree_parallelism, SCALE_FACTOR, seed);
+                wfc = new ParallelObserve(num_tries, rand, write_images, cur_max_degree_parallelism, SCALE_FACTOR, seed);
                 break;
             default:
-                wfc = new SequentialMain(num_trials, rand, write_images, SCALE_FACTOR, seed);
+                wfc = new SequentialMain(num_tries, rand, write_images, SCALE_FACTOR, seed);
                 break;
         }
 
         return wfc.Run(xnode, counter, screenshotNumber, watch);
     }
 
+    private static void InitializeAndLoad(string[] args)
+    {
+        try
+        {
+            write_images = Convert.ToBoolean(args[0]);
+            base_seed = Convert.ToInt32(args[1]);
+            num_trials = Convert.ToInt32(args[2]);
+            num_tries = Convert.ToInt32(args[3]);
+            SCALE_FACTOR = Convert.ToInt32(args[4]);
+
+            rand = new Random(base_seed);
+            replicate_seeds = new int[num_trials];
+            
+            for (int seed_num = 0; seed_num < num_trials; seed_num++)
+            {
+                replicate_seeds[seed_num] = rand.Next();
+            }
+            
+
+            int num_execution_modes = (args.Length - 5) / 3;
+
+            execution = new string[num_execution_modes];
+            max_degree_parallelism = new int[num_execution_modes];
+            num_workers = new int[num_execution_modes];
+
+            int i = 0;
+            for (int j = 5; j < args.Length; j += 3)
+            {
+                execution[i] = args[j];
+                max_degree_parallelism[i] = Convert.ToInt32(args[j + 1]);
+                num_workers[i] = Convert.ToInt32(args[j + 2]);
+                ValidExecutionName(execution[i], j);
+                i++;
+            }
+
+            rand = new Random(base_seed);
+            xdoc = new XmlDocument();
+
+            xdoc.Load("samples.xml");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\nUsage: MultithreadedWaveFunctionCollapse bool_write_images int_seed int_number_of_main_loop_iterations int_number_of_tries scale_factor " +
+                "execution_mode int_max_degree_of_parallelism int_number_of_workers ... " +
+                "(execution_mode int_max_degree_of_parallelism int_number_of_workers)\n");
+            Console.WriteLine(e);
+            /*
+            WaitForEscKey();
+            Environment.Exit(exitCode: 24);
+            */
+        }
+    }
+
+    private static bool ValidExecutionName(string v, int j)
+    {
+        switch (v)
+        {
+            case "sequential-main":
+                return true;
+            case "parallel-main":
+                return true;
+            case "parallel-propagate":
+                return true;
+            case "parallel-observe":
+                return true;
+            default:
+                Console.WriteLine(j + "th argument " + v + " is invalid");
+                Console.WriteLine("Use only one of the following: sequential-main, parallel-main, parallel-propagate, or parallel-observe");
+                Environment.Exit(1);
+                return false;
+        }
+    }
+
+    private static string[] DisplayTime(TimeSpan[] execution_times, int run_num)
+    {
+        Console.WriteLine();
+        ulong total_runtime = 0;
+        string[] lines = new string[execution.Length];
+        int replicate_seed = replicate_seeds[run_num];
+        for (int i = 0; i < execution.Length; i++)
+        {
+            Console.WriteLine(execution[i] + " execution time: " + execution_times[i]);
+            lines[i] = run_num + ", " + SCALE_FACTOR + ", " + execution[i] + ", " + base_seed + ", " + replicate_seed + ", " + max_degree_parallelism[i] + ", " +
+                num_workers[i] + ", " + num_tries + ", " + num_trials + ", " + execution_times[i];
+            total_runtime += (ulong) execution_times[i].TotalMilliseconds;
+        }
+
+        Console.WriteLine("Run #" + run_num + " Runtime: " + total_runtime + " ms\n");
+
+        return lines;
+    }
+
+    private static void WriteRuntimes(List<string[]> runs)
+    {
+        StringBuilder sb = new StringBuilder("Run Number, Scale Factor, Execution Name, Base Seed, Replicate Seed, Max Degree of Parallelism, Number of Workers, Number of Tries, Number of Trials, Runtime (ms)\n");
+
+        foreach (var runtimes in runs)
+        {
+            foreach (var runtime in runtimes)
+            {
+                sb.Append(runtime + "\n");
+            }
+        }
+
+        string path = Directory.GetCurrentDirectory() + "\\" + "MultithreadedWaveFunctionCollapseResults.csv";
+        System.IO.File.WriteAllText(path, sb.ToString());
+    }
+   
     private static void WaitForEscKey()
     {
         Console.WriteLine("\nPress ESC to stop");
@@ -258,11 +273,9 @@ internal abstract class WaveFunctionCollapse
         string ID_String = (id < 0) ? ("") : (" WITH " + id);
         for (int k = 0; k < _numTriesMainLoop; k++)
         {
-            Console.Write(">");
-            //int seed = _random.Next();
-            int seed = SEED;
+            Console.Write(">");            
             watch.Restart();
-            bool finished = model.Run(seed, xnode.Get("limit", 0));
+            bool finished = model.Run(SEED, xnode.Get("limit", 0));
             watch.Stop();
             if (finished)
             {
